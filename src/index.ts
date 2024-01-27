0dfab2465ac08380b063aba6639d6769d3654b9a
Nova2419 <shivamojha2419@gmail.com>
2024-01-27 21:43:58 +0530

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+const express = require('express');
 import router from './routes/router'
 
 const app = express();
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,9 +1,13 @@
-import { Router } from "express";
+const { Router } = require("express");
 
 const router = Router();
 
-router.get("/", (req,res) => {res.json({message: "Hello from docker!!"})})
+router.get("/", (req,res) => {
+ res.json({message: "Hello from docker!!"})
+})
 
-router.get("/health" , (req,res) => res.status(200).json({message: "Everything looks healthy here!!"}))
+router.get("/health" , (req,res) => {
+ res.status(200).json({message: "Everything looks healthy here!!"})
+})
 
 export default router
