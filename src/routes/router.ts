579502af19c9438954d1d8e104d23b23bcdcf3c6
Nova2419 <shivamojha2419@gmail.com>
2024-01-27 22:03:03 +0530

    Docker code updates

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,11 +2,11 @@ const { Router } = require("express");
 
 const router = Router();
 
-router.get("/", (req,res) => {
+router.get("/", (req: any,res: { json: (arg0: { message: string; }) => void; }) => {
  res.json({message: "Hello from docker!!"})
 })
 
-router.get("/health" , (req,res) => {
+router.get("/health" , (req: any,res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { message: string; }): void; new(): any; }; }; }) => {
  res.status(200).json({message: "Everything looks healthy here!!"})
 })
 
